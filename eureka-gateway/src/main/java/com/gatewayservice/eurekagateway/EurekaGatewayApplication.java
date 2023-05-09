cbf097c39a68b413b506bd1070dbd2ef145e8a47
Aleksandar <mozartkaraokeversion@gmail.com>
2023-05-09 15:35:56 +0200

    podeseno za prvo pokretanja [localhost reference umesto naziva servisa itd]

--- eureka-gateway/src/main/java/com/gatewayservice/eurekagateway/EurekaGatewayApplication.java
+++ eureka-gateway/src/main/java/com/gatewayservice/eurekagateway/EurekaGatewayApplication.java
@@ -33,8 +33,8 @@ public class EurekaGatewayApplication {
 		return "TEST GATEWAY";
 	}
 	
-	@Autowired
-	private RedisHashComponent redisHashComponent;
+//	@Autowired
+//	private RedisHashComponent redisHashComponent;
 	
 	
 //	@PostConstruct
