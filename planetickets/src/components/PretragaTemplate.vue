67d9402e32c49ef904907ce7cc35c4d50892a795
zoran96 <zoran.lukac.96@gmail.com>
2023-05-14 18:37:14 +0200

    Pregled smestaja na stranici za pretragu

---
+++ planetickets/src/components/PretragaTemplate.vue
@@ -0,0 +1,92 @@
+<template>
+    <!--SORTIRANJE-->
+    <p>{{this.listaSmestaja.brojDana}}</p>
+    <section style="margin-left: 10px; margin-bottom:100px;">
+        <div>
+            <div class="row">
+                <div class="col-md-4" v-bind:key="index" v-for="(sTemp, index) in sortEntities.slice(0,15)">
+                    <div style="width:90%;height:300px; margin-bottom:100px;" class="card">
+                       <img  :src="getAnImage(sTemp)" alt="card image collar"> 
+                       <div class="card-body">
+                        <h5 class="card-title">{{sTemp.adresa.adresa}}</h5>
+                        <p class="card-text">Ocena: {{sTemp.prosecnaOcena}}</p>
+                        <p class="card-text">Cena(ukupna): {{sTemp.ukCena}}</p>
+                        <p class="card-text">Cena(po Osobi): {{this.getProsekCena(sTemp.ukCena)}}</p>
+                        <button style="margin-left:30px;" v-on:click="smestajDet(sTemp.id)" class="btn btn-primary">Detalji</button>
+                       </div>
+                    </div>
+                </div>                        
+            </div>
+        </div>
+    </section>
+
+</template>
+<script>
+
+export default {
+    components: { },
+    props: [
+        'listaSmestaja',
+        
+    ],
+        data(){
+        return {
+            userObj: {},
+            currentSortDir: 'asc',
+            currentSort: 'polazakIz',
+            
+        }
+    },
+    methods: { 
+        getAnImage(smestaj){
+            let tmpImg = 'data:image/png;base64,';
+            for(let i = 0; i < smestaj.slike.length; i++){
+                tmpImg += smestaj.slike[i];
+                break;
+            }
+            console.log(tmpImg);
+            return tmpImg;
+        },
+        getProsekCena(cena){
+            return cena/this.listaSmestaja.brojDana
+        },
+        smestajDet(id){
+            this.$router.push(`/pregled/${id}`);
+        },
+    },
+    created(){
+        //if(parserMixin.methods.checkLoginStatus() == true){
+        console.log("ID KORISNIKA: " + this.userObj.role);
+        //}
+    },
+    computed:{
+        sortEntities(){
+            let sortirani = this.listaSmestaja.listaSmestaja;
+            return sortirani;
+        },
+    }
+
+}
+
+</script>
+<style scoped>
+.table {
+    font-family: Arial, Helvetica, sans-serif;
+    margin-left: 20px;
+
+}
+
+.table tr:nth-child(even) {
+    background-color: aquamarine;
+}
+
+.table td:nth-child(1) {
+    width: 30%;
+    font-size: 20px;
+    font-weight: 600;
+}
+
+.table td>input {
+    width: 100%;
+}
+</style>
